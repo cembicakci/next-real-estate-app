442eda1a7ed1b65f57ba02b070ef9ea7c17a4064
Cem <cmbicakci@gmail.com>
2022-10-25 19:41:06 +0300

    search page

---
+++ pages/components/SearchFilters.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next/image'
+
+function SearchFilters() {
+    return (
+        <div>SearchFilters</div>
+    )
+}
+
+export default SearchFilters
