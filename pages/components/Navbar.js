9dd9ce67a2f4591f18ae2ac95eb10e656ecceb3e
Cem <cmbicakci@gmail.com>
2022-10-25 18:29:28 +0300

    created Navbar, installed icons

---
+++ pages/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react'
+import { FcMenu, FcHome, FcAbout } from 'react-icons/fc'
+import { BsSearch } from 'react-icons/bs'
+import { FaKey } from 'react-icons/fi'
+
+function Navbar() {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar
