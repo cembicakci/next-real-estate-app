79dc4f42092e5be27f59dc9db869eef1abb4f961
Cem <cmbicakci@gmail.com>
2022-10-26 20:00:49 +0300

    filter data

--- pages/components/SearchFilters.js
+++ pages/components/SearchFilters.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
 
+import { filterData, getFilterValues } from '../../utils/filterData'
+
 function SearchFilters() {
+
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+
+    }
+
     return (
-        <div>SearchFilters</div>
+        <Flex background='gray.100' p='4' flexWrap='wrap' justifyContent='center'>
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                        placeholder={filter.placeholder}
+                        w='fit-content' p='2'>
+
+                        {filter?.items?.map((item => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        )))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     )
 }
 
